ba181a934c4038ffef78b109deede6c1d9086627
Armen Melik-Abramians <abramians2110@gmail.com>
2025-03-22 16:20:44 -0700

    Initial Commit

---
+++ FootballChallenge/FootballChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballChallengeApp.swift
+//  FootballChallenge
+//
+//  Created by Armen Melik-Abramians on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
